b86448a8e3ff535660a0133b5bde2b0bda86c4a1
printfdinul <dinullah2001@gmail.com>
2022-03-25 11:43:55 +0700

    Latihan1.php, view-latihan.php

--- application/views/view-latihan.php
+++ application/views/view-latihan.php
@@ -4,13 +4,9 @@
 	<title>Latihan 1</title>
 </head>
 <body>
-<?php 
-Halo Kawan.. Yuk kita belajar web programming..!!!<br>
- 
- Nilai 1 = <?= $nilai1; ?>
- Nilai 2 = <?= $nilai2; ?>
- ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
- .$nilai1 ." + " .$nilai2 ." = " .$hasil; ?>
-?>
+ Halo Kawan.. Yuk kita belajar web programming..!!!<br>
+ <h3>Nilai 1 =<?php= $nilai1; ?> </h3>
+ <h3>Nilai 2 =<?php= $nilai2; ?></h3>
+ ini hasil dari pemodelan dengan methode penjumlahan yaitu <?php= . $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
 </body>
 </html>
